7f5dda40ffc3d21422e9c3c9eef1f291fa90450d
Ryan-B-Runo <43120875+Ryan-B-Runo@users.noreply.github.com>
2025-06-03 21:21:14 -0500

    Do naive power function
    Including unit tests and timing

---
+++ Driver.java
@@ -0,0 +1,71 @@
+public class Driver {
+
+    public static int naivePower(int x, int n) {
+        if(x==0){
+            return 0;
+        }else if(n == 0){
+            return 1;
+        }
+        return x*naivePower(x, n-1);
+    }
+
+    public static int unoptimizedDCPower(int x, int n) {
+        if(x==0){
+            return 0;
+        }else if(n == 0){
+            return 1;
+        }else if(n % 2 == 0){
+            return unoptimizedDCPower(x, n/2) * unoptimizedDCPower(x, n/2);
+        }
+        else{
+            return x * unoptimizedDCPower(x, n/2) * unoptimizedDCPower(x, n/2);
+        }
+    }
+
+    public static int optimizedDCPower(int x, int n) {
+        if(x==0){
+            return 0;
+        }else if(n == 0){
+            return 1;
+        }
+        int temp = optimizedDCPower(x, n/2);
+        if(n % 2 == 0){
+            return temp * temp;
+        }else{
+            return x * temp * temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        //generate three different "power tables" (one for each function) and record the time taken to compute each data point.
+        long[][] naiveTable = new long[10][10];
+        long[][] unoptimizedDCTable = new long[10][10];
+        long[][] optimizedDCTable = new long[10][10];
+
+        int temp = 0;
+        long startTime = 0;
+        long endTime = 0;
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                startTime = System.nanoTime();
+                temp = naivePower(i, j);
+                endTime = System.nanoTime();
+                //unit tests to make sure the correct result is calculated
+                //also calculate time for performance analysis
+                if(temp == Math.pow(i, j)){
+                    naiveTable[i][j] = endTime - startTime;
+                }else{
+                    System.out.println("Error with " + i + "^" + j + " = " + temp + " not " + Math.pow(i, j));
+                }
+            }
+        }
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                System.out.print(naiveTable[i][j] + "     \t     ");
+            }
+            System.out.println();
+        }
+    }
+}
